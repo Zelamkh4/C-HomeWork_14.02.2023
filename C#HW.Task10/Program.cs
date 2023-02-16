7062b9027d4601f107bac892c9a990ef845c730d
Zelamkh4 <zelamkh.tutushev@icloud.com>
2023-02-16 11:30:40 -0800

    Add task10

---
+++ C#HW.Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, 
+//которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Clear();
+
+
+Console.Write("enter a three-digit number: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine("the second digit of this number -> "+stringNumber[1]);
