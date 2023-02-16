8e939e87359b65c432bf30b3bd08f496d170875b
Zelamkh4 <zelamkh.tutushev@icloud.com>
2023-02-16 11:35:39 -0800

    Add Task13

---
+++ C#HW.Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, 
+//которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет
+
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Enter number: ");
+
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+
+if (anyNumberText.Length > 2){
+  Console.WriteLine("third num -> " + anyNumberText[2]);
+}
+
+else {
+
+  Console.WriteLine("-> no third num");
+}
